0eae7a7f87c2dc85046b275ece3efe2189535d99
JakeBrose <mordecaistudios@gmail.com>
2018-10-18 13:55:29 -0500

    object instantiation, foreach, if practice

--- intro/Program.cs
+++ intro/Program.cs
@@ -6,10 +6,30 @@ namespace bangazon
     {
         static void Main(string[] args)
         {
+            // ------ 
             DateTime purchaseDate=DateTime.Now;
             string lastName="Brose";
             var firstName="Jake";
             Console.WriteLine($"{firstName} {lastName} purchased on {purchaseDate}");
+            // ------
+
+            // ------
+            string[] products = new string[] {
+                "Motorcycle",
+                "Sofa",
+                "Sandals",
+                "Omega Watch",
+                "iPhone"
+            };
+            foreach (string product in products) {
+                Console.WriteLine(product);
+            };
+            foreach (string product in products) {
+                if (product.Length > 5) {
+                    Console.WriteLine(product);
+                }
+            };
+            // ------
         }
     }
 }
